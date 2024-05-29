a3aeba2b1f58140aa5c2b5b10388cdfa534e9c83
maximilianoaguero <maximilianoaguero@aol.com>
2024-05-29 10:05:17 -0300

    cambios01

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express();
 const port = 4000;
 
-app.listen(port);
+app.listen(process.env.PORT||4000);
 console.log(`Servidor app escuchando en puerto${port}`);
